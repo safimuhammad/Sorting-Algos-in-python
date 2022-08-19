1f82b4d746cc72f4a1f8b7fd9a83e5b2a06e2b53
safimuhammad <safiwahid38@gmail.com>
2022-08-20 01:32:46 +0500

    insertion sort

--- binarysearch.py
+++ binarysearch.py
@@ -135,4 +135,26 @@ if __name__ == '__main__':
     print("Sorted array is: ", end="\n")
     printList(arr)
 
-#quick sort incoming
+# Insertion sort in Python
+
+
+def insertionSort(array):
+
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1
+        
+        # Compare key with each element on the left of it until an element smaller than it is found
+        # For descending order, change key<array[j] to key>array[j].        
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+        
+        # Place key at after the element just smaller than it.
+        array[j + 1] = key
+
+
+data = [9, 5, 1, 4, 3]
+insertionSort(data)
+print('Sorted Array in Ascending Order:')
+print(data)
